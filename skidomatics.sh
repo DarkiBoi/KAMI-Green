f00558e06d462b2f201eb6ee22157a2c7f6b0c9d
blockparole <>
2019-11-07 00:50:10 +0100

    less verbose paths

--- skidomatics.sh
+++ skidomatics.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
-git clone https://github.com/zeroeightysix/KAMI.git KAMI-OLD
-git clone https://github.com/S-B99/KAMI.git KAMI
+git clone https://github.com/zeroeightysix/KAMI.git KAMI
+git clone https://github.com/S-B99/KAMI.git KAMI-BLUE
 
-mv ./KAMI-OLD/build.gradle KAMI
-mv ./KAMI-OLD/gradle KAMI
-mv ./KAMI-OLD/gradle.properties KAMI
-mv ./KAMI-OLD/gradlew KAMI
+mv KAMI/build.gradle KAMI-BLUE
+mv KAMI/gradle KAMI-BLUE
+mv KAMI/gradle.properties KAMI-BLUE
+mv KAMI/gradlew KAMI-BLUE
 
-rm -rf ./KAMI-OLD
+rm -rf KAMI
 
-cd ./KAMI
+cd KAMI-BLUE
 
-sed -i 's/KAMI Blue/KAMI Green/g' ./src/main/resources/mcmod.info
-sed -i 's/KAMI Blue/KAMI Green/g' ./src/main/java/me/zeroeightsix/kami/KamiMod.java
+sed -i 's/KAMI Blue/KAMI Green/g' src/main/resources/mcmod.info
+sed -i 's/KAMI Blue/KAMI Green/g' src/main/java/me/zeroeightsix/kami/KamiMod.java
 
 ./gradlew build
 
-mv ./build/libs/KAMI-*-release.jar ../KAMI-Green.jar
+mv build/libs/KAMI-*-release.jar ../KAMI-Green.jar
 
 cd ..
 
-rm -rf KAMI
+rm -rf KAMI-BLUE
